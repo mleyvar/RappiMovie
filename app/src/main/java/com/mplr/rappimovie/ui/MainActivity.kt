2baed7799595ff13c28508abd6f8c1bf63988bdf
Alejandro Ochoa <alejandro.ochoa@rappi.com>
2021-08-11 22:40:12 -0500

    Add main composable

--- app/src/main/java/com/mplr/rappimovie/ui/MainActivity.kt
+++ app/src/main/java/com/mplr/rappimovie/ui/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.R
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -21,34 +22,24 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val viewModel = hiltViewModel<MainViewModel>()
-            viewModel.getPopularMovies(1)
+            RappiMovieApp()
+        }
+    }
+}
 
+
+@Composable
+fun RappiMovieApp() {
+    val viewModel = hiltViewModel<MainViewModel>()
+    viewModel.getPopularMovies(1)
+
+    RappiMovieTheme {
+        Surface(color = MaterialTheme.colors.background) {
             viewModel.popularMovies.observe(LocalLifecycleOwner.current) {
 
                 Toast.makeText(this, "Data Here:" + it.results.size, Toast.LENGTH_SHORT).show()
                 // para ver la imagen concatena  ( Const.BASE_IMAGE_URL + poster_path ) del objeto Movie
             }
-
-            RappiMovieTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    RappiMovieTheme {
-        Greeting("Android")
     }
 }
